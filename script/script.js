f66d5418f842e180e48ac4ede66b7e0db3fff6d6
MaksPzm <makspzm@gmail.com>
2025-03-09 03:56:21 +0500

    доделад head для всех заданных экранов, а так же мобильную кнопку меню

--- script/script.js
+++ script/script.js
@@ -1,15 +1,13 @@
 const mobMenuBtn = (() => {
-    const burgerMenu: any = document.querySelector(".header__mainMenu_menu_burger-menu_img");
-    const mobMenu: any = document.querySelector(".header__mainMenu_mob_list");
-    const mobMenuExit: any = document.querySelector(".header__mainMenu_mob_exitImg");
-
+    const burgerMenu = document.querySelector(".header__mainMenu_menu_burger-menu_img");
+    const mobMenu = document.querySelector(".header__mainMenu_mob_list");
+    const mobMenuExit = document.querySelector(".header__mainMenu_mob_exitImg");
     burgerMenu.addEventListener('click', () => {
         burgerMenu.classList.add("hidden");
         mobMenu.classList.remove("hidden");
-    })
-
+    });
     mobMenuExit.addEventListener('click', () => {
         mobMenu.classList.add('hidden');
         burgerMenu.classList.remove('hidden');
-    })
-})()
+    });
+})();
